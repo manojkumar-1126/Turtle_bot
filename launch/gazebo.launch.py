5e631678a6fa793fd7859d5a1c06142c39e6ec77
manoj kumar <manojkumar.doddi11@gmail.com>
2026-07-11 14:06:02 +0530

    updated

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import AppendEnvironmentVariable, TimerAction
+from launch.actions import AppendEnvironmentVariable
 
 def generate_launch_description():
 
